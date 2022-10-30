5685dc0357c80500e69b1e2eb30e2663377c09e2
Abubakar Ibrahim <abubakaribrahim603@gmail.com>
2022-10-30 20:35:34 +0100

    created my first ever frontend project

--- src/index.js
+++ src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const page = (
+  <div className="">
+    <div className>
+      <h1>
+        Fun facts about React:
+      </h1>
+      <ul>
+        <li>hello</li>
+        <li>suwaidah</li>
+        <li>abubakar</li>
+        <li>ibrahim</li>
+      </ul>
+    </div>
+  </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(page);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
